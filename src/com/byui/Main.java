3050263b41a4854ca98f4188e094da8a8cfcd456
jwatkinsm <wat18023@byui.edu>
2020-06-23 22:43:56 -0400

    classes added

--- src/com/byui/Main.java
+++ src/com/byui/Main.java
@@ -5,5 +5,14 @@ public class Main {
     public static void main(String[] args) {
 	// This is a test class.
         //test 123
+      /*
+      DandD creature/charcter creator
+      ---account savings and checking
+      cars
+      ---money management app
+      playing cards
+      ---currency converter
+      Pokemon scambler
+       */
     }
 }
