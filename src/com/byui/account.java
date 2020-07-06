2184f27bd2f9dca97648e5d24b4510d663bc40d9
jwatkinsm <wat18023@byui.edu>
2020-07-06 18:31:30 -0400

    account UML

--- src/com/byui/account.java
+++ src/com/byui/account.java
@@ -2,6 +2,23 @@ package com.byui;
 
 /**
  * UML
+ * ////note ?? might not be neccesary for this part of code////
+ *variables
+ *      acctnum: int
+ *      name: string
+ *      dateCreated: java.util.Date
+ *    ??  totalAmmount: double
+ *Constructors
+ *      Account()
+ *      Account(name: string, acctnum: int, ??totalAmount: double)
+ *getters/setters
+ *      getAcctnum(): int
+ *      getName(): string
+ *      getDateCreated(): java.util.Date
+ *    ??  getTotalAmmount(): double
+ *      setAcctnum(): void
+ *      setName(): void
+ *     ?? setTotalAmmount(): void
  *
  */
 public class account {
