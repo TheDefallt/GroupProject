051f9fbb4d386a4c0d9eb6e155e6b3ade099320f
TheDefallt <57537793+TheDefallt@users.noreply.github.com>
2020-07-14 12:34:49 -0700

    Update savings.java

--- src/com/byui/savings.java
+++ src/com/byui/savings.java
@@ -16,4 +16,38 @@ package com.byui;
  *
  */
 public class savings extends account {
+  //Stores the APR for the savings account.
+  private double annualPercentageYeild;
+  
+  //No-arg constructor
+  public Savings(){
+  
+  }
+  
+  //Parameterized constructor
+  public Savings(double balance, string name, double annualPercentageYeild){
+    this.balance = balance;
+    this.name = name;
+    this.annualPercentageYeild = annualPercentageYeild;
+  }
+  
+  //APR Getter
+  public double getAnnualPercentageYeild() {
+    return annualPercentageYeild;
+  }
+  
+  //APR Setter
+  public void setAnnualPercentageYeild(double annualPercentageYeild){
+    this.annualPercentageYeild = annualPercentageYeild;
+  }
+  
+  public double calculateAnnualBalance(){
+    return 
+  }
+  
+  
+  
+  
+  
+  
 }
