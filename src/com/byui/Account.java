81129b0fee079f5aba36aa3a6007d9df21755a60
jwatkinsm <wat18023@byui.edu>
2020-07-16 19:55:11 -0400

    main savings pesudo code

--- src/com/byui/Account.java
+++ src/com/byui/Account.java
@@ -48,6 +48,8 @@ public class Account {
 
     }
     //object
+
+
     public Account(String name, double balance){
         this.acctNum = acctNum;
         this.name = name;
--- src/com/byui/Main.java
+++ src/com/byui/Main.java
@@ -8,9 +8,9 @@ public class Main {
     public static void main(String[] args) {
 	//things needed
         //polymorphism
-        //printWriter(write)
-        //file(read)
-        //Array or Arraylist
+        //printWriter(write) check
+        //file(read) check
+        //Array or Arraylist check
 
         /**
          * Pesudo Code
@@ -35,10 +35,22 @@ public class Main {
          *               SHOW name, account Number,starting balance,total expense, and current balance
          *
          *      ELSE IF user INPUT is no
-         *          Proceed to Savings prompt
+         *          proceed to the rest of the program
          *      ElSE
          *         ERROR prompt user to enter yes no
-         *
+         * Ask the user if they Want to start a checking
+         *                IF user INPUT is yes
+         *                    * checking account info
+         *                         SCAN user input for Account Balance
+         *                         GENERATE account Number for user account
+         *                         SCAN user INPUT APR
+         *                         Calculate the worth of savings account in an year
+         *                         SHOW name, account Number,starting balance,APR, and projected balance
+         *                ELSE IF user INPUT is no
+         *                    proceed to the rest of the program
+         *                ElSE
+         *                   ERROR prompt user to enter yes no
+         * SHOW goodbye message
          */
     }
 }
