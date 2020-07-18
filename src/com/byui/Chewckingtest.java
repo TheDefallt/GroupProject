bb34084670d9f3ecafac17179940338124904089
jwatkinsm <wat18023@byui.edu>
2020-07-17 22:02:22 -0400

    account update

--- src/com/byui/Chewckingtest.java
+++ src/com/byui/Chewckingtest.java
@@ -8,18 +8,27 @@ public class Chewckingtest {
 
     public static void main(String[] args) {
         String name;
-        int  acctNum;
-        double balance = 0, income;
-        ArrayList<Double> expenses =new ArrayList<>();
+        char createOrNot;
+
         Scanner input= new Scanner(System.in);
-
-
 
        System.out.println(" Program info");
        System.out.print("Please enter you full name: ");
        name = input.nextLine();
-        Account.displayObject(new Checking(name,balance));
+       System.out.print(name + "Would you like to add an checking account Y/N?");
+       createOrNot= input.next().charAt(0);
+       if(createOrNot == 'y'|| createOrNot =='Y'){
+           createChecking(name);
+       }else{
+           System.out.println("input must be Y or N.");
+       }
     }
+    public static void createChecking( String name){
+        ArrayList<Double> expenses =new ArrayList<>();
+        int  acctNum;
+        double balance = 0, income;
 
+        Account.displayObject(new Checking(expenses,name,balance));
+    }
 
 }
